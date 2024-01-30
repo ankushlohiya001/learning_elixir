83c5d4f0701e757c35e6dfad5bb6a13ed1c2d358
ankushlohiya001 <ankushlohiya2000@gmail.com>
2024-01-30 19:50:31 +0530

    some testing :)

--- test/learning_elixir_test.exs
+++ test/learning_elixir_test.exs
@@ -2,14 +2,8 @@ defmodule LearningElixirTest do
   use ExUnit.Case
 
   test "stack test" do
-    st = %Stack{}
-    st = Stack.push(st, 34)
-    st = Stack.push(st, 37)
-    assert length(st.list) == 2
-    {st, top} = Stack.pop(st)
-    assert top == 37
-    assert length(st.list) == 1
-    {st, _top} = Stack.pop(st)
-    assert Stack.empty?(st)
+    infix = "A + B * C / (D - E)"
+
+    assert postfix(infix) == ""
   end
 end
